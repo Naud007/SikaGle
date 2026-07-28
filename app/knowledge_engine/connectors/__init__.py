f42fab30ddf0d49d54b7fc98a3d8ec8d1911e40d
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-07-28 14:40:22 +0100

    Update __init__.py

--- app/knowledge_engine/connectors/__init__.py
+++ app/knowledge_engine/connectors/__init__.py
@@ -1,14 +1,9 @@
 from .base import BaseConnector
 from .fao import FAOConnector
 from .inrab import INRABConnector
-from .registry import registry
-
-registry.register("fao", FAOConnector)
-registry.register("inrab", INRABConnector)
 
 __all__ = [
     "BaseConnector",
     "FAOConnector",
     "INRABConnector",
-    "registry",
 ]
