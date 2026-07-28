954869a032b7da0cd718e628d57618626fd7592c
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-07-28 13:54:18 +0100

    Update inrab_crawler.py

--- app/knowledge_engine/crawlers/inrab_crawler.py
+++ app/knowledge_engine/crawlers/inrab_crawler.py
@@ -1,1 +1,24 @@
+from __future__ import annotations
 
+from app.knowledge_engine.models import INRABPublication
+
+from .base_crawler import BaseCrawler
+
+
+class INRABCrawler(BaseCrawler[INRABPublication]):
+    """
+    Crawler du portail des publications INRAB.
+    """
+
+    BASE_URL = "https://publications-chercheurs.inrab.bj"
+
+    SEARCH_URL = (
+        "https://publications-chercheurs.inrab.bj/publications/recherche_simple"
+    )
+
+    def discover(self) -> list[INRABPublication]:
+        """
+        Sera implémenté lors de la prochaine étape.
+        """
+
+        return []
