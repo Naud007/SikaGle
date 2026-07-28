bfc68d42a472209ec7afee9ca1a7056412ee27d7
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-07-28 17:46:52 +0100

    Update __init__.py

--- app/knowledge_engine/harvesters/__init__.py
+++ app/knowledge_engine/harvesters/__init__.py
@@ -1,13 +1,1 @@
-from app.knowledge_engine.harvesters.brab_oai import (
-    BRABOAIHarvester,
-)
-from app.knowledge_engine.harvesters.registry import (
-    HarvesterRegistry,
-)
-
-registry = HarvesterRegistry()
-
-registry.register(
-    "brab",
-    BRABOAIHarvester,
-)
+# Ancien système de harvesters désactivé.
