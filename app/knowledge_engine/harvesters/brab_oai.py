7e0477456d98fdaea108d2211c355b9262f9e4eb
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-07-28 16:59:23 +0100

    Update brab_oai.py

--- app/knowledge_engine/harvesters/brab_oai.py
+++ app/knowledge_engine/harvesters/brab_oai.py
@@ -1,17 +1,11 @@
 from __future__ import annotations
 
-from app.knowledge_engine.protocols.oai.client import (
-    OAIClient,
-)
-from app.knowledge_engine.protocols.oai.normalizer import (
-    OAINormalizer,
-)
-from app.knowledge_engine.protocols.oai.parser import (
-    OAIParser,
+from app.knowledge_engine.harvesters.base_oai import (
+    BaseOAIHarvester,
 )
 
 
-class BRABOAIHarvester:
+class BRABOAIHarvester(BaseOAIHarvester):
     """
     Harvester OAI-PMH du BRAB.
     """
@@ -20,50 +14,4 @@ class BRABOAIHarvester:
         "https://brab.bj/index.php/brab/oai"
     )
 
-    def __init__(self):
-
-        self.client = OAIClient(
-            self.BASE_URL
-        )
-
-        self.parser = OAIParser()
-
-        self.normalizer = OAINormalizer()
-
-    def harvest(self):
-
-        documents = []
-
-        soup = self.client.list_records()
-
-        while True:
-
-            records = self.parser.parse_records(
-                soup
-            )
-
-            for record in records:
-
-                documents.append(
-                    self.normalizer.normalize(
-                        record,
-                        source="BRAB",
-                    )
-                )
-
-            token = (
-                self.parser.parse_resumption_token(
-                    soup
-                )
-            )
-
-            if not token:
-                break
-
-            soup = (
-                self.client.list_records_from_token(
-                    token
-                )
-            )
-
-        return documents
+    SOURCE = "BRAB"
