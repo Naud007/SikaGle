5a1daf2b59ea3891b49fe3a45fb970be8731c743
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-07-30 12:57:40 +0100

    Update __init__.py

--- app/knowledge_engine/retrieval/__init__.py
+++ app/knowledge_engine/retrieval/__init__.py
@@ -1,11 +1,13 @@
 from .hybrid_retriever import HybridRetriever
 from .keyword_retriever import KeywordRetriever
 from .search_engine import SearchEngine
+from .search_query import SearchQuery
 from .search_result import SearchResult
 from .vector_retriever import VectorRetriever
 
 __all__ = [
     "SearchResult",
+    "SearchQuery",
     "VectorRetriever",
     "KeywordRetriever",
     "HybridRetriever",
