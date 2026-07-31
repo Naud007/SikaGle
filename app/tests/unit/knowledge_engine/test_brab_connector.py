9e24c8500e2e2fa0408ad3272d5878bbb80eed2b
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-07-31 20:25:02 +0100

    Update test_brab_connector.py

--- app/tests/unit/knowledge_engine/test_brab_connector.py
+++ app/tests/unit/knowledge_engine/test_brab_connector.py
@@ -1,1 +1,29 @@
+from app.knowledge_engine.connectors.brab import BRABConnector
 
+
+def test_connector_name():
+
+    connector = BRABConnector()
+
+    assert connector.name == "brab"
+
+
+def test_connector_has_client():
+
+    connector = BRABConnector()
+
+    assert connector.client is not None
+
+
+def test_connector_has_parser():
+
+    connector = BRABConnector()
+
+    assert connector.parser is not None
+
+
+def test_connector_has_normalizer():
+
+    connector = BRABConnector()
+
+    assert connector.normalizer is not None
