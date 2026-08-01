6a204369cadeabfa2374e6c75c2ce29efe3d2477
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-08-01 16:38:25 +0100

    Update translation_service.py

--- app/multimodal/translation/translation_service.py
+++ app/multimodal/translation/translation_service.py
@@ -1,1 +1,26 @@
+from app.multimodal.models.translation import (
+    Translation,
+)
+from app.multimodal.translation.translator import (
+    Translator,
+)
 
+
+class TranslationService:
+
+    def __init__(self):
+
+        self.translator = Translator()
+
+    def translate(
+        self,
+        text: str,
+        source_language: str,
+        target_language: str,
+    ) -> Translation:
+
+        return self.translator.translate(
+            text=text,
+            source_language=source_language,
+            target_language=target_language,
+        )
