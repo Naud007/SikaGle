bc356501c206a9c6430262e8e477d56a37e9481f
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-08-01 23:54:44 +0100

    Update logger.py

--- app/core/logging/logger.py
+++ app/core/logging/logger.py
@@ -1,1 +1,27 @@
+import logging
+import sys
 
+
+def setup_logger():
+
+    logger = logging.getLogger("SikaGle")
+
+    logger.setLevel(logging.INFO)
+
+    if logger.handlers:
+        return logger
+
+    formatter = logging.Formatter(
+        "[%(asctime)s] %(levelname)s | %(name)s | %(message)s"
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+logger = setup_logger()
