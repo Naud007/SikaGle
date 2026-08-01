9298ece9e5a10c7b6c768f05a5672a147df68f13
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-08-01 16:27:52 +0100

    Update voice_selector.py

--- app/multimodal/speech/voice_selector.py
+++ app/multimodal/speech/voice_selector.py
@@ -1,1 +1,17 @@
+class VoiceSelector:
 
+    DEFAULT_VOICES = {
+        "fr": "fr-FR-Standard-A",
+        "fon": "fon-default",
+        "yo": "yo-NG-Standard-A",
+    }
+
+    def select(
+        self,
+        language: str,
+    ) -> str:
+
+        return self.DEFAULT_VOICES.get(
+            language,
+            self.DEFAULT_VOICES["fr"],
+        )
