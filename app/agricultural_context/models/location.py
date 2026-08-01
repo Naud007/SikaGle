3a0d9795eb62123047a634386d3034caf4b6d866
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-08-01 15:33:35 +0100

    Update location.py

--- app/agricultural_context/models/location.py
+++ app/agricultural_context/models/location.py
@@ -1,1 +1,17 @@
+from __future__ import annotations
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Location:
+
+    country: str
+
+    department: str | None = None
+
+    commune: str | None = None
+
+    latitude: float | None = None
+
+    longitude: float | None = None
