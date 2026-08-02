0a438b8b5f52785a42e4376f69fa4e1325a41b9f
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-08-02 17:05:52 +0100

    Update __init__.py

--- app/core/security/__init__.py
+++ app/core/security/__init__.py
@@ -5,7 +5,13 @@ Module de sécurité.
 """
 
 from .security_settings import SecuritySettings
+from .secret_validator import (
+    SecretValidator,
+    SecretValidationError,
+)
 
 __all__ = [
     "SecuritySettings",
+    "SecretValidator",
+    "SecretValidationError",
 ]
