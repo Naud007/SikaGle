0ad5a72edba847d467f6ba78d25d8347c891a7c1
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-08-02 16:46:20 +0100

    Update __init__.py

--- app/core/monitoring/__init__.py
+++ app/core/monitoring/__init__.py
@@ -1,1 +1,17 @@
+"""
+SikaGlé
+
+Module de monitoring de la plateforme.
+"""
+
 from .metrics import metrics
+from .alert_levels import AlertLevel
+from .alerts import Alert
+from .alert_manager import AlertManager
+
+__all__ = [
+    "metrics",
+    "AlertLevel",
+    "Alert",
+    "AlertManager",
+]
