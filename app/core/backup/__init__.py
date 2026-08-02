4d694c1eeb8df2468a98c97b4f59efe0db2ced7d
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-08-02 16:58:51 +0100

    Update __init__.py

--- app/core/backup/__init__.py
+++ app/core/backup/__init__.py
@@ -1,11 +1,15 @@
 """
 SikaGlé
 
-Module de sauvegarde.
+Module de sauvegarde de la plateforme.
 """
 
 from .backup_type import BackupType
+from .backup_report import BackupReport
+from .backup_manager import BackupManager
 
 __all__ = [
     "BackupType",
+    "BackupReport",
+    "BackupManager",
 ]
