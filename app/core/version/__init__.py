6f80d0a61770b89430c7d3f9bb1b2305ebf09fda
Arnaud A. <121523660+Naud007@users.noreply.github.com>
2026-08-02 18:37:24 +0100

    Update __init__.py

--- app/core/version/__init__.py
+++ app/core/version/__init__.py
@@ -4,8 +4,10 @@ SikaGlé
 Gestion des versions.
 """
 
+from .maintenance_report import MaintenanceReport
 from .version_info import VersionInfo
 
 __all__ = [
     "VersionInfo",
+    "MaintenanceReport",
 ]
