d7355f50f8c4519eae3d495a3ca30b1801bbe5ee
Naud007 <arnaudshop4@gmail.com>
2026-08-31 16:12:12 +0100

    fix: enregistre AfricaRiceConnector dans connectors/__init__.py (connecteur invisible car jamais importe au demarrage)

--- app/knowledge_engine/connectors/__init__.py
+++ app/knowledge_engine/connectors/__init__.py
@@ -1,23 +1,26 @@
 from .base import BaseConnector
 from .brab import BRABConnector
 from .fao import FAOConnector
+from .africarice import AfricaRiceConnector
 from .registry import registry
-
 
 registry.register(
     "fao",
     FAOConnector,
 )
-
 registry.register(
     "brab",
     BRABConnector,
 )
-
+registry.register(
+    "africarice",
+    AfricaRiceConnector,
+)
 
 __all__ = [
     "BaseConnector",
     "FAOConnector",
     "BRABConnector",
+    "AfricaRiceConnector",
     "registry",
 ]
